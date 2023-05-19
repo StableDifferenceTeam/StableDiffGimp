976fb75fed8477722a3e8a9feba5a1d47016e639
FridtjofS <93829965+FridtjofS@users.noreply.github.com>
2023-05-19 15:31:31 +0200

    add simple workflow descriptions

--- src/main.py
+++ src/main.py
@@ -1,34 +1,48 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
-import os
-import sys
 import gimpfu
 
-# main function to be called by GIMP
+
+def my_python_function(timg, tdrawable):
+    first_layer = timg.layers[0]
+    first_layer.name = "my first action"
 
 
-# Just a dummy function to test the plugin, it does nothing
-# the plugin should have a button next to file, edit, select, view, etc.
-# and it should be called "Hello World"
+register(
+    proc_name=("my_gimp_name"),
+    blurb=("short description"),
+    help=("long description"),
+    author=("author"),
+    copyright=("owner"),
+    date=("year"),
+    label=(""),
+    imagetypes=("*"),
+    params=[
+        (PF_IMAGE, "")
+    ]
+    results=[]
+    function=(unsere func die wir noch machen),
+    menu=("<Image>/Filters/YourChoice")
+    domain=("gimp20-python", gimp.locale_directory)
+)
+main()
 
-if __name__ == "__main__":
 
-    def plugin_main():
-        pass
+# Workflow for Main (like in the main of stableboy):
 
-    # This is the plugin registration function
-    gimpfu.register(
-        "python_fu_hello_world",
-        "Hello World",
-        "Hello World",
-        "Your Name",
-        "Your Name",
-        "2019",
-        "<Image>/Filters/Artistic/Hello World",
-        "",  # type of image it works on (*, RGB, RGB*, RGBA, GRAY etc...)
-        "",
-        [],  # type of drawable it works on (IMAGE, VISIBLE_LAYERS, etc...)
-        [],  # other parameters
-        plugin_main)  # callback function
+# def is_cmd() simple function to check if we are in cmd or not (just precaution)
 
-    gimpfu.main()
+# get all files from folder .stableboy\commands into cmd_module_locations[]
+
+# initialize empty list with registered commands registered_cmds[] to avoid double registration (just precaution)
+
+# for each obj in cmd_module_locations[]:
+#  if obj not in registered_cmds[] and obj.is_cmd():
+#   run command via command_runner
+#   obj.register()
+#   registered_cmds.append(obj)
+
+
+# In stable Boy stable diffusion and other commands are differentiated,
+# but thats stuff we gotta worry about later i'd say.
+# other commands include setting Preferences, etc.
