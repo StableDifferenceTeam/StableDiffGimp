76ac86ea47c6c3e23a8b1e9adb361c54a3709530
Peter Isbrandt <peter@isbrandt-calle.de>
2023-06-11 15:21:05 +0200

    changed Expert Mode to Toggle in extra window

--- src/stabledifference/commands/text-to-image.py
+++ src/stabledifference/commands/text-to-image.py
@@ -1,32 +1,50 @@
 # text-to-image command
 import gimpfu
 import stabledifference as sdiff
+from gimpshelf import shelf
 from Command import StableDiffusionCommand
 
 
 class TextToImageCommand(StableDiffusionCommand):
-    uri = "sdapi/v1/txt2img"
-    metadata = StableDiffusionCommand.CommandMetadata(
+    uri = "sdapi/v1/text-to-image"
+
+    if shelf.hasKey('expert_mode') & shelf['expert_mode']:
+        metadata = StableDiffusionCommand.CommandMetadata(
         "TextToImageCommand",
         "Stable Boy " + sdiff.__version__ + " - Text to Image",#"StableDifference Text to Image Command " + "v1",  # sdiff.__version__,
         "StableDiffusion Plugin for GIMP",
         "StableDifference",
         "StableDifference",
         "2023",
-        "<Image>/StableDifference/Text to Image/Expert mode",  # menu path
+        "<Image>/StableDifference/Text to Image",  # menu path
         "*", [
             (gimpfu.PF_STRING, "prompt", "Prompt", "Enter your Prompt here", ""),
             (gimpfu.PF_STRING, "negative_prompt", "Negative Prompt", ""),
             (gimpfu.PF_STRING, 'seed', 'Seed', '-1'),
             (gimpfu.PF_SLIDER, 'steps', 'Steps', 25, (1, 150, 25)),
             (gimpfu.PF_OPTION, 'sampler_index',
-             'Sampler', 0, sdiff.constants.SAMPLERS),
+            'Sampler', 0, sdiff.constants.SAMPLERS),
             (gimpfu.PF_BOOL, 'restore_faces', 'Restore faces', False),
             (gimpfu.PF_SLIDER, 'cfg_scale', 'CFG', 7.5, (0, 20, 0.5)),
             (gimpfu.PF_SLIDER, 'num_images', 'Number of images', 1, (1, 4, 1)),
             (gimpfu.PF_OPTION, 'img_target', 'Results as',
-             0, sdiff.constants.IMAGE_TARGETS),
-            
+            0, sdiff.constants.IMAGE_TARGETS),
+
         ],
         [],
-    )
+        )
+    else:
+        metadata = StableDiffusionCommand.CommandMetadata(
+            "TextToImageSimpleCommand",
+            "StableDifference Text to Image (Simplified) Command " + "v1",  # sdiff.__version__,
+            "StableDiffusion Plugin for GIMP",
+            "StableDifference",
+            "StableDifference",
+            "2023",
+            "<Image>/StableDifference/Text to Image/Simple mode",  # menu path
+            "*", [
+                (gimpfu.PF_STRING, "prompt", "Prompt", "Enter your Prompt here", ""),
+                (gimpfu.PF_STRING, "negative_prompt", "Negative Prompt", ""),
+            ],
+            [],
+            )
