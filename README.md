baeba3e80999a21533633be0e4f09923a6ab2448
Johannes von Kleist <80823398+JohannesvKL@users.noreply.github.com>
2023-07-07 12:56:12 +0200

    Outcrop works

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # StableDiffGimp
 ## Description
+***
 This is a plugin for GIMP that allows you to incorporate Stable Diffusion into your workflow. It is based on the [AUTOMATIC1111's Stable Diffusion WebUI](https://github.com/AUTOMATIC1111/stable-diffusion-webui).
+
+## Installation
+***
+1. Download GIMP or verify, that you own the newest version of GIMP. 
+2. Clone [The Stable Diff Gimp Repository] (https://github.com/PeterIsbrandt/StableDiffGimp.git) by typing `git clone https://github.com/PeterIsbrandt/StableDiffGimp.git` in your terminal.
+3. Go to GIMP and add the Plugin to the list of plugins:
+    - Open GIMP
+    - Click on `Settings`
+    - Click on `Folder`
+    - Click on `Plugins`
+    - Click on `Add a new folder`
+    - Type in the location of your StableDiffGimp src folder. It should look something like: `/Users/.../StableDiffGimp/src`
+    - Close GIMP
+    - Open it again and verify, that you have a StableDiffGimp button
+4. Start Stable Diffusion
+    - You can start it on your own PC. If you do that, the link in the settings (to be implemented) should be: 
+
+
 
 
 
--- src/stabledifference/commands/simple-inpainting.py
+++ src/stabledifference/commands/simple-inpainting.py
@@ -9,7 +9,7 @@ class ImageToImageCommand(StableDiffusionCommand):
     metadata = StableDiffusionCommand.CommandMetadata(
         "ImageToImageCommand",
         #"Stable Boy " + sdiff.__version__ + " - Text to Image",
-        "StableDifference " + sdiff.__version__ + ": Inpainting",
+        "StableDifference " + sdiff.__version__ + ": Inpainting - Simple mode",
         "StableDiffusion Plugin for GIMP",
         "StableDifference",
         "StableDifference",
@@ -17,18 +17,7 @@ class ImageToImageCommand(StableDiffusionCommand):
         "<Image>/StableDifference/Inpainting",  # menu path
         "*", [
             (gimpfu.PF_STRING, "prompt", "Prompt", "", ""),
-            (gimpfu.PF_SLIDER, 'denoising_strength',
-             'Denoising Strength %', 50, (0, 100, 1)),
-            (gimpfu.PF_STRING, "negative_prompt", "Negative Prompt", ""),
-            (gimpfu.PF_STRING, 'seed', 'Seed', '-1'),
-            (gimpfu.PF_SLIDER, 'steps', 'Steps', 25, (1, 150, 25)),
-            (gimpfu.PF_OPTION, 'sampler_index',
-             'Sampler', 0, sdiff.constants.SAMPLERS),
-            (gimpfu.PF_BOOL, 'restore_faces', 'Restore faces', False),
-            (gimpfu.PF_SLIDER, 'cfg_scale', 'CFG', 7.5, (0, 20, 0.5)),
-            (gimpfu.PF_SLIDER, 'num_images', 'Number of images', 1, (1, 4, 1)),
-            (gimpfu.PF_OPTION, 'img_target', 'Results as',
-             0, sdiff.constants.IMAGE_TARGETS),
+            
 
         ],
         [],
